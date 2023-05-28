1a0932e56980bbf4e371db74eb1bb8b3117275d3
bananabrann <plbrannan95@gmail.com>
2023-05-28 13:56:54 -0500

    Add basic footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Footer from "./lib/Footer.svelte";
 </script>
 
 <main>
@@ -20,26 +21,31 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+
+  <Footer />
 </main>
 
-<style>
+<style lang="scss">
   .logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
+
     transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+    &:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    &.svelte:hover {
+      filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
   }
   .read-the-docs {
     color: #888;
