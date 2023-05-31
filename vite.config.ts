20cd3a953323ebd7f1c27ff1e590dea7169c9c95
bananabrann <plbrannan95@gmail.com>
2023-05-31 14:10:24 -0500

    Sveltekit boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "src/variables.scss" as *;',
-      },
-    },
-  },
+	plugins: [sveltekit()]
 });
